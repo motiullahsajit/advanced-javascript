89ef6b1fc2ef46878bb2b34595aacf5b68998726
Motiullah Sajit <sajitmotiullah@gamil.com>
2021-02-02 04:42:13 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,38 @@
+//undefiend 
+let pakhi;
+console.log(pakhi);
+//
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return
+}
+const result = add(13, 82);
+console.log(result);
+//
+function add2(num1, num2) {
+    console.log(num1, num2);
+    return
+}
+const result2 = add2(13);
+console.log(result2);
+//
+const premik = { name: "smart dude", phone: 458421 };
+console.log(premik.gf);
+//not recommended
+let fun = undefined;
+console.log(fun);
+//
+let ages = [2, 5, 9];
+console.log(ages[11]);
+
+
+//null=empty
+// null is set any elements value null
+let sum = 50;
+if (sum < 100) {
+    sum = null;
+    console.log(sum);
+}
+else {
+    console.log(sum);
+}
